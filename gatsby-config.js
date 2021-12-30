581dbaeb8cb2448300fddcdb76e006b8b4c2faae
Bill Jensen <jensendesign@users.noreply.github.com>
2021-12-29 20:40:06 -0500

    adding styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-postcss`,
     `gatsby-plugin-sass`,
+    // 'gatsby-plugin-css-customs',
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
